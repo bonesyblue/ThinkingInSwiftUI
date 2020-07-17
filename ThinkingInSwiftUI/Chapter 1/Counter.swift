feba4fd2e2d63cb4d0761439b20c3234c3351c58
bonesyblue <bonesyblue@gmail.com>
2020-07-17 17:16:36 +0200

    feat: Add boilerplate SwiftUI project

---
+++ ThinkingInSwiftUI/Chapter 1/Counter.swift
@@ -0,0 +1,21 @@
+//
+//  Counter.swift
+//  ThinkingInSwiftUI
+//
+//  Created by Jonathan Bones on 17.07.20.
+//  Copyright © 2020 bonesyblue. All rights reserved.
+//
+
+import SwiftUI
+
+struct Counter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Counter_Previews: PreviewProvider {
+    static var previews: some View {
+        Counter()
+    }
+}
---
+++ ThinkingInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThinkingInSwiftUI
+//
+//  Created by Jonathan Bones on 17.07.20.
+//  Copyright © 2020 bonesyblue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
